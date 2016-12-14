2775ca8e3fabf1e9e47a928c7208bb4fe58e419d
hatemLW <hatem@logicsware.com>
2016-12-14 16:09:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ app.post('/webhook/', function (req, res) {
             //sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
             sendTextMessage(sender,  text.substring(0, 200));
         }
+            if(event)
+               console.log (event.toString());
     }
     res.sendStatus(200);
 });
