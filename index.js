e0133fea3283635ff680d4ff66bea4fd988dcfba
hatemLW <hatem@logicsware.com>
2016-12-20 13:23:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.use(bodyParser.json());
 //app.set('port', (FB_PORT || 5000));
 
 app.set('port', (process.env.PORT || 5000));
-//app.set('verify_token', (process.env.VERIFY_TOKEN || 'TEST'));
-//app.set('page_access_token', (process.env.PAGE_ACCESS_TOKEN || 'NULL'));
+app.set('verify_token', (process.env.VERIFY_TOKEN || 'TEST'));
+app.set('page_access_token', (process.env.PAGE_ACCESS_TOKEN || 'NULL'));
 
 
 var PAGE_ACCESS_TOKEN = 'EAAOlPqyA6G8BACwqDoewkvsQCUtimjsbIbCpl7CeuDhhABJNb20itWtBAAlTVdb8vaPQU7WXnV7Pgw41iZCvUw0nJv6lZA4JV2j1bvpZBxZBkSApXZA0fJ0gS3ckLZAS5MXIFXOKLyakZCXK2FRYacMZC139NYY5Ncfo6ZAoVEyvZALTY1XF3lF2ZCU';
@@ -30,7 +30,7 @@ app.get('/', function (req, res) {
 
 app.get('/webhook', function (req, res) {
         //if (req.query['hub.verify_token'] === app.get('verify_token')) {
-        if (req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {           
+        if (req.query['hub.verify_token'] === app.get('verify_token')) {           
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Error, wrong validation token');
@@ -92,7 +92,7 @@ function sendTextMessage(sender, text) {
     });
 }
 
-app.listen(FB_PORT, function() {
+app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
